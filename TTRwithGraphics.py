c1abfc1da7227a485893343ec3c9f3fa706f4a87
lvastel <93648791+lvastel@users.noreply.github.com>
2021-11-04 10:17:07 +0100

    Update TTRwithGraphics.py

--- TTRwithGraphics.py
+++ TTRwithGraphics.py
@@ -7,6 +7,7 @@ from math import*
 loop = 1
 while loop == TRUE:
     ##basic settings and questions
+    taille = int(input('what size window?'))
     num = int(input('how many numbers?'))
     TT = int(input('what times table?'))
     ##color choice
@@ -14,18 +15,19 @@ while loop == TRUE:
     color_2 = input('which color lines?')
 
     ##represent window and circle
-    win = GraphWin('Times Tables Representation', 700, 700, autoflush=FALSE)
+    win = GraphWin('Times Tables Representation', taille, taille, autoflush=FALSE)
     win.setBackground(color_1)
-    win.setCoords(-350,-350,350,350)
-    cercle = Circle(Point(0,0), 300)
+    win.setCoords(-taille/2,-taille/2,taille/2,taille/2)
+    radius = taille/2 - 50
+    cercle = Circle(Point(0,0), radius)
     cercle.draw(win)
     ##draw lines
     for i in range(num):
         w = TT * i
-        P1x = 300*(math.sin((pi/180)*(i*(360/num)+90)))
-        P1y = 300*(math.cos((pi/180)*(i*(360/num)+90)))
-        P2x = 300*(math.sin((pi/180)*(w*(360/num)+90)))
-        P2y = 300*(math.cos((pi/180)*(w*(360/num)+90)))
+        P1x = radius*(math.sin((pi/180)*(i*(360/num)+90)))
+        P1y = radius*(math.cos((pi/180)*(i*(360/num)+90)))
+        P2x = radius*(math.sin((pi/180)*(w*(360/num)+90)))
+        P2y = radius*(math.cos((pi/180)*(w*(360/num)+90)))
         ligne = Line(Point(P1x,P1y),Point(P2x,P2y))
         ligne.setOutline(color_2)
         ligne.draw(win)
